469fe73f45040e464d092e49409f91492940256e
ThunderAnimal <MartinWeber.93@outlook.com>
2017-05-18 14:05:21 +0200

    adds input parameter to main

--- serial/head_conduction.c
+++ serial/head_conduction.c
@@ -1,10 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "core.h"
 
 int main(int argc, char **argv)
 {
-    head_conduction_simple(100, 0.05, 15, 0.25);
+    float diffEndTemperature;
+    unsigned int rasterSize;
+    float waermeleitfaehigkeit;
+    int debug;
 
+    if(argc < 4){
+        fprintf(stderr, "Notenough arguments given!\n");
+        return EXIT_FAILURE;
+    }
+
+    diffEndTemperature = (float) atof(argv[1]);
+    rasterSize = (unsigned int) atoi(argv[2]);
+    waermeleitfaehigkeit = (float) atof(argv[3]);
+    if(argc == 4){
+        debug = 0;
+    }else{
+        debug = atoi(argv[4]);
+    }
+    int steps = head_conduction_simple(diffEndTemperature, rasterSize, waermeleitfaehigkeit, debug);
+
+    printf("#############\nENDE: %d\n", steps);
     return EXIT_SUCCESS;
 }
