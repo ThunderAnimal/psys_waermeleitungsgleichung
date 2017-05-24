1d1a180eeb732476ef94da35d997e449b6b3e36a
ThunderAnimal <MartinWeber.93@outlook.com>
2017-05-24 09:42:08 +0200

    Fix gui - use jquery from node_modules - use hammerjs from node_modules - use materialize from  node_modules - use local material fonts

---
+++ gui/app/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by insanemac on 24/05/2017.
+ */
+$( document ).ready(function() {
+    // Handler for .ready() called.
+    $("#btnStart").click(function () {
+        alert("Starte Verarbeitung");
+    });
+
+});
